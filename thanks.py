32e47059bd3d9e0484cbd09f4fd0e98fc349c48a
Trevor Suarez <rican7@gmail.com>
2015-02-05 03:08:13 -0500

    Smarter querying and better error handling

--- thanks.py
+++ thanks.py
@@ -23,10 +23,13 @@ thankyou_messages = [
     'Thank you! I appreciate it!'
 ]
 
+# Define our error-based sleep fallback timer
+sleep_time = 1
+
 
 # Define our ridiculous "birthday" query
 def build_birthday_fql(min_date, max_date, limit = 500):
-    birthday_fql = ("SELECT post_id, actor_id, target_id, created_time, message, comments "
+    birthday_fql = ("SELECT post_id, actor_id, target_id, created_time, message, comments, like_info "
                     "FROM stream "
                     "WHERE source_id = me() "
                         "AND filter_key = 'others' "
@@ -35,6 +38,7 @@ def build_birthday_fql(min_date, max_date, limit = 500):
                         "AND actor_id != me() "
                         "AND comments.count = 0 "
                         "AND comments.can_post = 1 "
+                        "AND like_info.can_like = 1 "
                         "AND (strpos(message, 'birthday') >= 0 "
                             "OR strpos(message, 'Birthday') >= 0 "
                             "OR strpos(message, 'happy') >= 0 "
@@ -64,7 +68,7 @@ birthday_current_year = datetime.datetime(
     birthdate.day
 )
 
-fql = build_birthday_fql(birthday_current_year, datetime.datetime.utcnow(), 500)
+fql = build_birthday_fql(birthday_current_year, datetime.datetime.utcnow(), 100)
 
 # Grab our birthday posts using our FQL query
 birthday_posts = graph.fql(fql)
@@ -81,23 +85,28 @@ for post in birthday_posts:
     # Grab the post's ID
     post_id = post['post_id']
 
-    # "Like" the post
-    graph.put_object(post_id, 'likes')
-
     # Get a random message from the list
     rand_message = random.choice(thankyou_messages)
 
-    # Post the comment on the post
-    graph.put_object(post_id, 'comments', message=rand_message)
+    try:
+        if post['like_info']['user_likes'] != True:
+            # "Like" the post
+            graph.put_object(post_id, 'likes')
 
-    # Increment our counter..
-    posts_responded_to += 1
+        # Post the comment on the post
+        graph.put_object(post_id, 'comments', message=rand_message)
 
-    # Print to keep track
-    print 'The like/comment should have posted for post', post_id
+        # Increment our counter..
+        posts_responded_to += 1
 
-    # Sleep for a bit to try and keep from getting rate limited
-    sleep(0.1) # Sleep for a tenth of a second
+        # Print to keep track
+        print 'The like/comment should have posted for post', post_id
+    except facebook.GraphAPIError as e:
+        print 'Error on responding to post with message:', e.message
+        print 'Sleeping for', sleep_time, 'second(s)'
+
+        # Sleep for a bit to try and keep from getting rate limited
+        sleep(sleep_time)
 
 # Let's get this "likes" steez going
 
