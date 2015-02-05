32bc3a837173d4b570835b438fda64dd0c8a9266
Trevor Suarez <rican7@gmail.com>
2015-02-05 02:44:49 -0500

    Now dynamically building the FQL query based on the current user's birthday

--- thanks.py
+++ thanks.py
@@ -4,6 +4,7 @@
 import facebook
 import json # Grab our JSON lib too
 import os
+import datetime
 import random
 from time import sleep
 
@@ -22,29 +23,51 @@ thankyou_messages = [
     'Thank you! I appreciate it!'
 ]
 
+
 # Define our ridiculous "birthday" query
-birthday_fql = ("SELECT post_id, actor_id, target_id, created_time, message, comments "
-                "FROM stream "
-                "WHERE source_id = me() "
-                    "AND filter_key = 'others' "
-                    "AND created_time > 1391346000 "
-                    "AND actor_id != me() "
-                    "AND comments.count = 0 "
-                    "AND comments.can_post = 1 "
-                    "AND (strpos(message, 'birthday') >= 0 "
-                        "OR strpos(message, 'Birthday') >= 0 "
-                        "OR strpos(message, 'happy') >= 0 "
-                        "OR strpos(message, 'Happy') >= 0) "
-                "LIMIT 500")
+def build_birthday_fql(min_date, max_date, limit = 500):
+    birthday_fql = ("SELECT post_id, actor_id, target_id, created_time, message, comments "
+                    "FROM stream "
+                    "WHERE source_id = me() "
+                        "AND filter_key = 'others' "
+                        "AND created_time > {} "
+                        "AND created_time < {} "
+                        "AND actor_id != me() "
+                        "AND comments.count = 0 "
+                        "AND comments.can_post = 1 "
+                        "AND (strpos(message, 'birthday') >= 0 "
+                            "OR strpos(message, 'Birthday') >= 0 "
+                            "OR strpos(message, 'happy') >= 0 "
+                            "OR strpos(message, 'Happy') >= 0) "
+                    "LIMIT {}")
+
+    return birthday_fql.format(
+        int((min_date - datetime.datetime.utcfromtimestamp(0)).total_seconds()),
+        int((max_date - datetime.datetime.utcfromtimestamp(0)).total_seconds()),
+        limit
+    )
+
 
 # Create a new GraphAPI instance with our access token
 graph = facebook.GraphAPI(access_token)
 
-# Grab our birthday posts using our FQL query
-query_result = graph.get_object('fql', q=birthday_fql)
+# Get the user's birthdate
+birthdate = datetime.datetime.strptime(
+    graph.get_object('me')['birthday'],
+    '%m/%d/%Y'
+)
 
-# Grab the data from the response
-birthday_posts = query_result['data']
+# Get the user's birthday with the current year
+birthday_current_year = datetime.datetime(
+    datetime.datetime.utcnow().year,
+    birthdate.month,
+    birthdate.day
+)
+
+fql = build_birthday_fql(birthday_current_year, datetime.datetime.utcnow(), 500)
+
+# Grab our birthday posts using our FQL query
+birthday_posts = graph.fql(fql)
 
 # Report how many posts we found...
 print 'Query returned', len(birthday_posts), 'results'
